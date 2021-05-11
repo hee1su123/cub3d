75f6193613464476fb90cf5dc804691030521870
hee1su123 <orcawhale@hanmail.net>
2021-05-11 20:24:32 +0900

    coding gnl3

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,38 @@
 #include "cub3d.h"
 
+int	split_save(char **save_ad, char **line, int nl)
+{
+	char	*temp;
+
+	(*save_ad)[nl] = '\0';
+	if (!(*line = ft_strdup(*save_ad)))
+		return (-1);
+	if (!(ft_strlen(*save_ad + nl + 1)))
+	{
+		free(*save_ad);
+		*save_ad = 0;
+		return (1);
+	}
+	if (!(temp = ft_strdup(*save_ad + nl + 1)))
+		return (-1);
+	free(*save_ad);
+	*save_ad = temp;
+	return (1);
+}
+
+int	check_nextline(char *save)
+{
+	int	i;
+
+	i = 0;
+	while (save[i] != 0 && save[i] != '\n')
+		i++;
+	if (save[i] == '\n')
+		return (i);
+	else
+		return (-1);
+}
+
 int	get_next_line(int fd, char **line, int error)
 {
 	static char	*save;
@@ -19,7 +52,12 @@ int	get_next_line(int fd, char **line, int error)
 	while ((read_len = read(fd, buff, BUFFER_SIZE)) > 0)
 	{
 		buff[read_len] = '\0';
-		temp = backup;
-		backup = ft_strjoin(temp, buff);
-		if ((loc_newline = check_newline(temp)) >= 0)
-
+		if (!(save = ft_strjoin(save, buff)))
+			return (-1);
+		if ((loc_nextline = check_nextline(save)) >= 0)
+			return(split_save(&save, line, loc_nextline));
+	}
+	if (read_len < 0)
+		return (-1);
+	return (return_gnl(&save, line));
+}
